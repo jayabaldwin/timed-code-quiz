871febc681ec0bf4317b96c0244dac28fe222342
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2023-12-14 17:36:34 +1100

    more javascript, pseudocode highscores page

---
+++ assets/js/highscores.js
@@ -0,0 +1,12 @@
+var highscoreList = document.getElementById("highscores-list");
+var resetButton = document.getElementById("reset-highscores");
+
+// scores need to be saved into an array
+
+// scores need to be locally accessed "getItem" "letItem"
+
+// scores need to be printed into var high score list 
+
+// scores need to present in descending order as its a leader board (a, b)
+
+// scores can be cleared using reset button
--- assets/js/script.js
+++ assets/js/script.js
@@ -31,51 +31,84 @@ var startButton = document.getElementById("start-button");
 var countdownEl = document.getElementById("countdown");
 var resultsPage = document.getElementById("results");
 
-// timer
+// starting timer state
 var timeLeft = 60;
 
 // question index
 var questionIndex = 0;
 
+
 // button that initiates the start game function
 startButton.addEventListener("click", startGame);
 
-// hides start screen, displays question screen, initiates timer and renders first question
+
 function startGame() {
+    // hides start screen
     startScreen.classList.add('hide');
+
+    // displays question screen
     questionContainer.classList.remove('hide');
 
-    setInterval(countdownTimer, 1000);
+    // initiates timer
+    countdownTimer();
 
+    // renders first question
     renderQuestions();
 }
 
-// timer... need to clearInterval when 0 but can't figure out how to when setInterval hasnt been declared as a function
+
+// timer
 function countdownTimer() {
-    timeLeft --;
-    countdownEl.textContent = timeLeft;
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+        // decrease var timeLeft that was declared earlier by 1
+        timeLeft --;
+
+        // countdown html element display the text of timeleft 
+        countdownEl.textContent = timeLeft;
+
+        if(timeLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+
+            // when time runs out, go to results page regardless of if quiz is completed
+            questionContainer.classList.add('hide');
+            resultsPage.classList.remove('hide');
+         }
+  
+     }, 1000);
+  }
 
 
-    if (timeLeft === 0) {
-        questionContainer.classList.add('hide');
-        resultsPage.classList.remove('hide');
-    }
-}
-
-
+// questions function
 function renderQuestions() {
+    // select question object from the array of questions
     var currentQuestion = questions[questionIndex];
+
+    // clear out previous question from html
     questionOptions.innerHTML = "";
+
+    // the text for the html question header = title of the current question 
     questionTitle.textContent = currentQuestion.title
 
+    // question array [indexed] and selecting the options property from inside it
     var optionsArray = questions[questionIndex].options;
+
+    // loop through each potential answer
     for (var i=0; i < optionsArray.length; i++) {
         var choice = optionsArray[i];
         console.log(choice);
 
+        // create buttons for each option
         var choiceEl = document.createElement("button");
+
+        // set the value of the button to be the choice variable aka the option
         choiceEl.setAttribute("value", choice);
+
+        // set the text content of the button to be the option values
         choiceEl.textContent = choice;
+
+        // adds button into the div
         questionOptions.appendChild(choiceEl);
     }
 }
@@ -97,16 +130,30 @@ function questionClick(event) {
     }
 }
 
-
-
-
-
-
-
-
-
-
 questionOptions.addEventListener("click", questionClick);
 
 
+
+
+
+
+
 // compare value, if correct all g, incorrect subtract 10, if value of counter 0 stop game
+
+// initially used within start game function but couldnt figure out how to clearinterval when it wasnt declared as a variable
+// setInterval(countdownTimer, 1000);
+
+// timer... need to clearInterval when 0 but can't figure out how to when setInterval hasnt been declared as a function
+// function countdownTimer() {
+//     // decrease var timeLeft that was declared earlier by 1
+//     timeLeft --;
+
+//     // countdown html element display the text of timeleft 
+//     countdownEl.textContent = timeLeft;
+
+//     // when time runs out, go to results page regardless of if quiz is answered
+//     if (timeLeft === 0) {
+//         questionContainer.classList.add('hide');
+//         resultsPage.classList.remove('hide');
+//     }
+// }
