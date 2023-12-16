5a83cd48725271a713f3b94cfbb59a1244056dd0
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2023-12-16 19:04:13 +1100

    added css, unfortunately broke some js code at same time, will debug now that css is complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // 4 questions to do... always start with data, then logic
 var questions = [
     {
-        title: "Which is NOT a primitive data type in JavaScript?",
-        options: ["Boolean", "Object", "Undefined", "String"],
-        answer: "Object"
+        title: "What is JavaScript primarily used for?",
+        options: ["Styling web pages", "Enhancing interactivity on web pages", "Database management", "Server-side scripting"],
+        answer: "Enhancing interactivity on web pages"
     },
     {
         title: "Which symbol represents the modulus?",
@@ -19,6 +19,26 @@ var questions = [
         title: "Which symbol is used for commenting out single-lines in JavaScript?",
         options: ["//", "/*...*/", "&&", "||"],
         answer: "//"
+    },
+    {
+        title: "Which is NOT a primitive data type in JavaScript?",
+        options: ["Boolean", "Object", "Undefined", "String"],
+        answer: "Object"
+    },
+    {
+        title: "What does the 'DOM' stand for in JavaScript?",
+        options: ["Document Object Model", "Data Object Model", "Document Order Model", "Dynamic Object Model"],
+        answer: "Document Object Model"
+    },
+    {
+        title: "What is the purpose of the addEventListener method in JavaScript?",
+        options: ["To add a new HTML element", "To attach an event handler to an element", "To create a new event", "To remove an event from an element"],
+        answer: "To attach an event handler to an element"
+    },
+    {
+        title: "What does the NaN value represent in JavaScript?",
+        options: ["Not a Number", "Null and Nothing", "Negative Number", "No Assignment"],
+        answer: "Not a Number"
     },
 ]
 
@@ -40,11 +60,18 @@ var timeLeft = 60;
 // question index
 var questionIndex = 0;
 
-// timer for clearing later
-var timerInterval;
+// timer 
+var timerInterval = setInterval(function() {
+    // decrease var timeLeft that was declared earlier by 1
+    timeLeft --;
+
+    // countdown html element display the text of timeleft 
+    countdownEl.textContent = timeLeft;
+}, 1000);
 
 // how many questions correct
 var correctQuestions;
+
 
 
 // button that starts the game
@@ -63,14 +90,9 @@ startButton.addEventListener("click", function() {
 });
 
 
-function countdownTimer() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-        // decrease var timeLeft that was declared earlier by 1
-        timeLeft --;
 
-        // countdown html element display the text of timeleft 
-        countdownEl.textContent = timeLeft;
+function countdownTimer() {
+    timerInterval;
 
         if(timeLeft === 0) {
             // Stops execution of action at set interval
@@ -80,8 +102,8 @@ function countdownTimer() {
             questionContainer.classList.add('hide');
             resultsPage.classList.remove('hide');
          }
-     }, 1000);
-  }
+     }
+
 
 
 function renderQuestions() {
@@ -116,10 +138,14 @@ function renderQuestions() {
 }
 
 
+
 function questionClick(event) {
     // Get the element that triggered the event and store it in buttonEl
     var buttonEl = event.target;
     console.log("answerValue", buttonEl.value);
+
+    // ADD IN IF STATEMENT, IF WHAT IS CLICKED IS NOT A BUTTON DO NOTHING
+   
 
     // Get the correct answer for the current question and store it in correctAnswer
     var correctAnswer = questions[questionIndex].answer;
@@ -129,6 +155,7 @@ function questionClick(event) {
         timeLeft -= 10;
     } else if 
         (buttonEl.value !== correctAnswer && timeLeft < 10) {
+            // THIS LINE IS OBSELETE, WE WANT IT TO = 0
         clearInterval(timerInterval);  
     }
 
@@ -149,6 +176,7 @@ function questionClick(event) {
 questionOptions.addEventListener("click", questionClick);
 
 
+
 submitButton.addEventListener("click", function() {
     var initials = initialsEl.value.trim();
     if (initials !== '') {
@@ -158,8 +186,6 @@ submitButton.addEventListener("click", function() {
             // SCORE SHOULD BE HOW MANY QUESTIONS ARE CORRECT + TIME REMAINING
             score: timeLeft
         };
-
-        finalScore.textContent = newScore.score;
 
         highScores.push(newScore);
         window.localStorage.setItem('highScores', JSON.stringify(highScores));
